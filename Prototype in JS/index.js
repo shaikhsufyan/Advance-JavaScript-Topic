dab3c60c0903a28242ed43634558daf49327f4fb
shaikh sufyan <shaikhsufyan1811@gmail.com>
2024-09-16 10:06:08 +0530

    Prototype in JavaScript

---
+++ Prototype in JS/index.js
@@ -0,0 +1,49 @@
+ 
+let arr = ["AMAN"]
+//  let obj = {
+//     name:"Shaikh",
+//     proff:"SE",
+//     getInfo:function(){
+//         console.log(`Name is ${this.name} and Proff is ${this.proff}`);
+        
+//     }
+//  }
+   
+//  let obj2={
+//     channel:"CODE With SS",
+     
+//  }
+//  obj2.__proto__=obj;
+//  console.log(obj2.getInfo());
+ 
+
+ 
+
+ function myFun(){
+
+ }
+
+ Function.prototype.sufyanFun = function(){
+    console.log("Sufyan Method is Running!!");
+    
+ }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
